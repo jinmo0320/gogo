731299064316026b19de645393e9b14d41309103
bokwang <chdl2353@naver.com>
2023-01-25 17:01:48 +0900

    Make map show current coords
    Add REAMDME.md

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,19 +1,32 @@
-import { useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 const { kakao } = window;
+const defaultCoords = [33.450701, 126.570667];
 
 function Map() {
-  useEffect(() => {
-    const container = document.getElementById("map"); //지도를 담을 영역의 DOM 레퍼런스
-    const options = {
-      //지도를 생성할 때 필요한 기본 옵션
-      center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
-      level: 3, //지도의 레벨(확대, 축소 정도)
-    };
+  const [coords, setCoords] = useState(defaultCoords);
+  const mapRef = useRef(null);
+  const container = useRef(null);
 
-    const map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
-  });
-  return <div id="map" style={{ width: "500px", height: "400px" }}></div>;
+  useEffect(() => {
+    const targetCoords = new kakao.maps.LatLng(...coords);
+    mapRef.current?.setCenter(targetCoords);
+  }, [coords]);
+
+  useEffect(() => {
+    const options = {
+      center: new kakao.maps.LatLng(...defaultCoords),
+      level: 3, //지도의 확대, 축소 정도
+    };
+    const map = new kakao.maps.Map(container.current, options);
+    mapRef.current = map;
+
+    navigator.geolocation.getCurrentPosition(
+      ({ coords: { latitude, longitude } }) => setCoords([latitude, longitude])
+    );
+  }, []);
+
+  return <div ref={container} style={{ width: "500px", height: "400px" }} />;
 }
 
 export default Map;
