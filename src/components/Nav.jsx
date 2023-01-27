d5070859a4c44e2847524743a37064dc0c8debdc
최진모 <jinmo@choejinmoui-MacBookAir.local>
2023-01-27 22:38:38 +0900

    nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import SvgIcon from "@mui/material/SvgIcon";
 import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
 import SearchIcon from "@mui/icons-material/Search";
